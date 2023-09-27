08ae1b7dc3508d5afca77a5e77e4e3e7346d58b7
vachaa <146204973+vachaa@users.noreply.github.com>
2023-09-28 00:59:45 +0700

    Add files via upload
    PBO-Latihan18

---
+++ PBO4182201007941822010082Latihan18.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo.pkg41822010079.pkg41822010082.latihan.pkg18;
+
+/**
+ *
+ * @author Caca
+ * Nama : Adjeng Rara Rengganis
+ * NIM  : 41822010079
+ * Nama : Vanisha Putri Kania
+ * NIM  : 41822010082
+ */
+public class PBO4182201007941822010082Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Format Tugas PBO");
+       
+        for (double i = 3.5; i <= 35.0; i += 3.5) {
+            if(i % 3.5 == 0 || 0x0 == i % 0x5)
+            System.out.println(i);
+        }
+
+        System.out.println(); 
+    }
+}
+
+
+       
+
+// TODO code application logic here
+    
+    
+
